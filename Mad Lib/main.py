8e1ab6411f1c286cc82af9d226fbf2328ad50234
Abraham Vanderpool <abevanderpool228@gmail.com>
2021-01-01 19:27:31 -0500

    Add files via upload

---
+++ Mad Lib/main.py
@@ -0,0 +1,7 @@
+print("Welcome to Mad Libs! Enter responses when you are prompted")
+verb = input("Enter a verb > ")
+adjective = input("Enter a adjective > ")
+noun = input("Enter a noun > ")
+sentence = f"The {adjective} {noun} {verb} the town's folk"
+
+print(sentence)
